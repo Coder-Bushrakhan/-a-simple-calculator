2e71a8f7bca9fbec3dbe870b43473a3affb7dab2
Coder-Bushrakhan <129618644+Coder-Bushrakhan@users.noreply.github.com>
2023-04-11 11:47:44 +0500

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,49 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+import chalk from "chalk";
+async function run() {
+    const answer = await inquirer.prompt([
+        {
+            name: "num1",
+            type: "number",
+            message: "Enter First Number:",
+            prefix: ">>"
+        },
+        {
+            name: "num2",
+            type: "number",
+            message: "Enter Second Number:",
+            prefix: ">>"
+        },
+        {
+            name: "operator",
+            type: "list",
+            message: "Enter Operator:",
+            choices: ["+", "-", "*", "/", "%", "^"],
+            prefix: ">>",
+        }
+    ]);
+    const num1 = Number(answer.num1);
+    const num2 = Number(answer.num2);
+    let result;
+    if (answer.operator == "+") {
+        result = num1 + num2;
+    }
+    else if (answer.operator == "-") {
+        result = num1 - num2;
+    }
+    else if (answer.operator == "*") {
+        result = num1 * num2;
+    }
+    else if (answer.operator == "/") {
+        result = num1 / num2;
+    }
+    else if (answer.operator == "%") {
+        result = num1 % num2;
+    }
+    else if (answer.operator == "^") {
+        result = num1 ** num2;
+    }
+    console.log(chalk.green(`Your Answer is ${result}`));
+}
+run();
